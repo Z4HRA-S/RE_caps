63b3cdab2e4ae9957537cf19e2f541afd567c46c
Zahra <iamzahra@protonmail.ch>
2022-12-06 13:01:30 +0330

    change decoder and num capsule

--- caps_net.py
+++ caps_net.py
@@ -1,3 +1,4 @@
+# https://github.com/jindongwang/Pytorch-CapsuleNet/blob/master/capsnet.py
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -36,8 +37,17 @@ class ConvLayer(nn.Module):
 class PrimaryCaps(nn.Module):
     def __init__(self, num_capsules=12, in_channels=256, out_channels=32, kernel_size=9):
         super(PrimaryCaps, self).__init__()
+        capsule = torch.nn.Sequential(
+            torch.nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
+                            kernel_size=1, stride=1, padding=0),
+            torch.nn.ReLU(),
+            torch.nn.Conv1d(in_channels=out_channels, out_channels=out_channels,
+                            kernel_size=kernel_size, stride=2, padding=0)
+        )
         self.capsules = nn.ModuleList([
-            nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=2, padding=0)
+            # nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
+            # kernel_size=kernel_size, stride=2, padding=0)
+            capsule
             for _ in range(num_capsules)])
 
     def forward(self, x):
@@ -54,7 +64,7 @@ class PrimaryCaps(nn.Module):
 
 
 class DigitCaps(nn.Module):
-    def __init__(self, num_capsules=10, num_routes=32 * 72, in_channels=12, out_channels=16):
+    def __init__(self, num_capsules=96, num_routes=32 * 72, in_channels=12, out_channels=16):
         super(DigitCaps, self).__init__()
 
         self.in_channels = in_channels
@@ -93,37 +103,40 @@ class DigitCaps(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_width=1, input_height=768, input_channel=3):
+    def __init__(self, input_height=768, input_channel=3, num_caps=96):
         super(Decoder, self).__init__()
-        self.input_width = input_width
+        self.num_caps = num_caps
         self.input_height = input_height
         self.input_channel = input_channel
         self.reconstraction_layers = nn.Sequential(
-            nn.Linear(16 * 10, 512),
+            nn.Linear(16 * num_caps, 512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 1024),
             nn.ReLU(inplace=True),
-            nn.Linear(1024, self.input_height * self.input_width * self.input_channel),
+            nn.Linear(1024, self.input_height * self.input_channel),
             nn.Sigmoid()
         )
 
-    def forward(self, x, data):
+    def forward(self, x, data, th):
         classes = torch.sqrt((x ** 2).sum(2))
-        classes = F.softmax(classes, dim=0)
-
-        _, max_length_indices = classes.max(dim=1)
-        masked = Variable(torch.sparse.torch.eye(10))
+        # classes = F.softmax(classes, dim=0)
+        # _, max_length_indices = classes.max(dim=1)
+        # masked = Variable(torch.sparse.torch.eye(self.num_caps))
+        masked = torch.zeros_like(classes)
+        indices = classes.gt(th)
+        masked[indices] = 1
         if USE_CUDA:
             masked = masked.cuda()
-        masked = masked.index_select(dim=0, index=Variable(max_length_indices.squeeze(1).data))
+        # masked = masked.index_select(dim=0, index=Variable(max_length_indices.squeeze(1).data))
+        masked = masked.squeeze(-1)
         t = (x * masked[:, :, None, None]).view(x.size(0), -1)
         reconstructions = self.reconstraction_layers(t)
-        reconstructions = reconstructions.view(-1, self.input_channel, self.input_width, self.input_height)
+        reconstructions = reconstructions.view(-1, self.input_channel, self.input_height)
         return reconstructions, masked
 
 
 class CapsNet(nn.Module):
-    def __init__(self, config=None):
+    def __init__(self, config=None, th=0.5):
         super(CapsNet, self).__init__()
         if config:
             self.conv_layer = ConvLayer(config.cnn_in_channels, config.cnn_out_channels, config.cnn_kernel_size)
@@ -139,13 +152,14 @@ class CapsNet(nn.Module):
             self.decoder = Decoder()
 
         self.mse_loss = nn.MSELoss()
+        self.th = th
 
     def forward(self, data):
         output = self.conv_layer(data)
         output = self.primary_capsules(output)
         output = self.digit_capsules(output)
-        # reconstructions, masked = self.decoder(output, data)
-        return output  # , reconstructions, masked
+        reconstructions, masked = self.decoder(output, data, self.th)
+        return output, reconstructions, masked
 
     def loss(self, data, x, target, reconstructions):
         return self.margin_loss(x, target) + self.reconstruction_loss(data, reconstructions)
@@ -155,9 +169,8 @@ class CapsNet(nn.Module):
 
         v_c = torch.sqrt((x ** 2).sum(dim=2, keepdim=True))
 
-        left = F.relu(0.9 - v_c).view(batch_size, -1)
-        right = F.relu(v_c - 0.1).view(batch_size, -1)
-
+        left = F.relu(self.th - v_c).view(batch_size, -1)
+        right = F.relu(v_c - self.th).view(batch_size, -1)
         loss = labels * left + 0.5 * (1.0 - labels) * right
         loss = loss.sum(dim=1).mean()
 
