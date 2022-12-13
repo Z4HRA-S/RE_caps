3436856678faedfb67431cfb26a30138e460cb4f
Zahra <iamzahra@protonmail.ch>
2022-12-13 18:19:27 +0330

    based on change in model, replace masked with output

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ def train_loop(dataloader, model, optimizer):
     size = len(dataloader.dataset)
     for batch, data in enumerate(dataloader):
         # Compute prediction and loss
-        loss, masked, labels = model(data)
+        loss, output, labels = model(data)
 
         torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
 
@@ -15,16 +15,15 @@ def train_loop(dataloader, model, optimizer):
         optimizer.step()
 
         if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(data)
-            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+            print(f"loss: {loss.item():>7f} [{(batch + 1) * len(data):>5d}/{size:>5d}]")
     return loss
 
 
 def test_loop(dataloader, model):
     with torch.no_grad():
         for data in dataloader:
-            loss, masked, labels = model(data)
-            f1 = model.f1_measure(masked, labels)
+            loss, output, labels = model(data)
+            f1 = model.f1_measure(output, labels)
 
     print(f"Test Error: \n F1: {(f1):>7f},  loss: {loss:>8f} \n")
     return loss
