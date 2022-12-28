c2307efc684981a8432fcb51a7ca5d650c4e8cb3
Zahra <iamzahra@protonmail.ch>
2022-12-28 11:02:31 +0330

    add attention and lstm

--- model.py
+++ model.py
@@ -15,6 +15,7 @@ class Model(nn.Module):
         self.embedding_model.to(device)
         self.caps_net = CapsNet(num_class=96, device=device)
         self.caps_net.to(device)
+        self.lstm = torch.nn.LSTM(768, 384, 2, bidirectional=True, device=device)
         self.device = device
 
     def forward(self, x):
@@ -27,25 +28,26 @@ class Model(nn.Module):
             output_attentions=True)
 
         feature_set, labels = self.extract_feature(embedded_doc, x)
-        output = self.caps_net(feature_set)
+        output, (hn, cn) = self.lstm(feature_set)
+        output = self.caps_net(output)
         return output, labels
 
     def get_pred(self, output):
-        norms =output.squeeze().norm(dim=-1)
-        #indices=norms.max(dim=-1)[1]
-        #preds = torch.nn.functional.one_hot(indices,num_classes=96)
+        norms = output.squeeze().norm(dim=-1)
+        # indices=norms.max(dim=-1)[1]
+        # preds = torch.nn.functional.one_hot(indices,num_classes=96)
         preds = norms.gt(0.5).float()
         return preds
 
     def extract_feature(self, embedded_doc, x):
         entities = x["entity_list"]
-        entity_list = self.aggregate_entities(entities, embedded_doc.last_hidden_state)
+        entity_list, attentions = self.aggregate_entities(entities, embedded_doc)
         cls_tokens = embedded_doc.last_hidden_state[:, 0]
 
         feature_set = []
         labels = []
 
-        for i, entities_embedding in enumerate(entity_list):
+        for i, (entities_embedding, attention_vector) in enumerate(zip(entity_list, attentions)):
             label = x["label"][i].to(self.device)
             if self.use_negative:
                 all_possible_ent_pair = self.all_possible_pair(len(entities_embedding))
@@ -57,7 +59,8 @@ class Model(nn.Module):
                     torch.stack([
                         cls_tokens[i],
                         entities_embedding[h],
-                        entities_embedding[t]
+                        entities_embedding[t],
+                        attention_vector[h] * attention_vector[t]
                     ])
                     for h, t in all_possible_ent_pair]
             )
@@ -79,21 +82,32 @@ class Model(nn.Module):
         labeled = [(i, j) for i in range(num_ent) for j in range(num_ent) if torch.sum(labels[i][j]) > 0]
         return labeled
 
-    def aggregate_entities(self, vertexSet_list: list,
-                           embedded_doc_list: torch.Tensor) -> list:
-        """
-        :param vertexSet_list: list of mentions positions in a doc in shape (batch, max_entity, max_mentions, 2)
-        :param embedded_doc_list: embedded doc in shape (batch, 512, 768)
-        :return: The logsumexp pooling of mentions for each entity in shape(max_entity, 768)
-        """
+    def aggregate_entities(self, vertexSet_list: list, embedded_doc_list):
         batch = []
-        for vertexSet, embedded_doc in zip(vertexSet_list, embedded_doc_list):
+        context_att = []
+        embedding = embedded_doc_list.last_hidden_state
+
+        # batch_size * token * token
+        last_attention_lr = torch.mean(embedded_doc_list.attentions[-1], dim=1)
+
+        for vertexSet, embedded_doc, attention in zip(vertexSet_list, embedding, last_attention_lr):
             mention_positions = [list(filter(lambda x: x[0] <= 512 and x[1] <= 512, ent)) for ent in vertexSet]
             mention_positions = [list(filter(lambda x: len(x) > 0, mnt)) for mnt in mention_positions]
             mention_positions = list(filter(lambda x: len(x) > 0, mention_positions))
+
+            context = [
+                torch.mean(
+                    torch.matmul(
+                        torch.concat([attention[pos[0] + 1:pos[1] + 1] for pos in mnt]), embedded_doc),
+                    dim=0)
+                for mnt in mention_positions]
             #  doc contains aggregated entity embedding, in shape(ent, 768)
             doc = [torch.logsumexp(torch.concat([embedded_doc[pos[0] + 1:pos[1] + 1] for pos in mnt]), dim=0)
                    for mnt in mention_positions]
+
             doc = torch.stack(doc)
             batch.append(doc)
-        return batch
+
+            context = torch.stack(context)
+            context_att.append(context)
+        return batch, context_att
